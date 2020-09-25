2688723ae1985678b4cff2984951b169d698dc2e
mahdi jafari <m.jafari1379@gmail.com>
2020-09-26 00:01:45 +0330

    Added code

--- source.cpp
+++ source.cpp
@@ -2,21 +2,17 @@
 
 using namespace std;
 
-int freeCell, mainBoard[3][3];
-
 void pause() {
     cout << "\nPress enter...";
     cin.ignore().get();
 }
 
-void init(int board[3][3]) {
-    system("clear"); //Use system("cls") for windows os
-    freeCell = 9;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            board[i][j] = 0;
-        }
+int fi(int n) {
+    int f = 1;
+    for (n; n > 1; n--) {
+        f *= n;
     }
+    return f;
 }
 
 void draw(int board[3][3]) {
@@ -41,16 +37,29 @@ void draw(int board[3][3]) {
     }
 }
 
-int tileOfNum(int n) {
+int tileOfNumI(int n) {
+    if(n == 7) {return 0;}    
+    if(n == 8) {return 0;}
+    if(n == 9) {return 0;}
+    if(n == 4) {return 1;}
+    if(n == 5) {return 1;}
+    if(n == 6) {return 1;}
+    if(n == 1) {return 2;}
+    if(n == 2) {return 2;}
+    if(n == 3) {return 2;}
+    return 0;
+}
+
+int tileOfNumJ(int n) {
     if(n == 7) {return 0;}    
     if(n == 8) {return 1;}
     if(n == 9) {return 2;}
-    if(n == 4) {return 10;}
-    if(n == 5) {return 11;}
-    if(n == 6) {return 12;}
-    if(n == 1) {return 20;}
-    if(n == 2) {return 21;}
-    if(n == 3) {return 22;}
+    if(n == 4) {return 0;}
+    if(n == 5) {return 1;}
+    if(n == 6) {return 2;}
+    if(n == 1) {return 0;}
+    if(n == 2) {return 1;}
+    if(n == 3) {return 2;}
     return 0;
 }
 
@@ -82,50 +91,64 @@ int checkWinner(int board[3][3]) {
     return 0;
 }
 
-void playerMove() {
-    int input, i, j;
-    while(true) {
-        cin >> input;
-        i = tileOfNum(input) / 10;
-        j = tileOfNum(input) % 10;
-        if(board [i][j] == 0) {
-            if(freeCell % 2 == 0) {
-                board [i][j] = 2;
-            } else {
-                board [i][j] = 1;
-            }
-            freeCell--;
-            break;
-        }
-        draw();
-        cout << "The tile is unavalible!\n";
-    }
-}
-
 int main() {
-    int mode, turn;
+    int mode, turn, freeCell, input, board [3][3];
     while(true) {
-        init();
+        system("clear"); //Use system("cls") for windows os
+        freeCell = 9;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = 0;
+            }
+        }
 
         cout << "Controlls:\nChoose the tile by keyboard's numpad.\n\nWhich player (1 or 2) you want to be? ";
         cin >> mode;
 
-        while(freeCell > 0 && checkWinner() == 0) {
-            draw();
+        while(freeCell > 0 && checkWinner(board) == 0) {
+            draw(board);
             if(mode % 2 == freeCell % 2) {
-                playerMove();
+                while(true) {
+                    cin >> input;
+                    if(board [tileOfNumI(input)][tileOfNumJ(input)] == 0) {
+                        if(freeCell % 2 == 0) {
+                            board [tileOfNumI(input)][tileOfNumJ(input)] = 2;
+                        } else {
+                            board [tileOfNumI(input)][tileOfNumJ(input)] = 1;
+                        }
+                        freeCell--;
+                        break;
+                    }
+                }
             } else {
-                //cpuMove(mode);
-                playerMove();
+                int chooseTile;
+                for(int tile = 1; tile <= 9; tile++) {
+                    int grade = 0;
+                    if(board [tileOfNumI(tile)][tileOfNumJ(tile)] == 0) {
+                        int newGrade = getGrade(tile);
+                        if(newGrade > grade) {
+                            grade = newGrade;
+                            chooseTile = tile;
+                        }
+                    }
+                }
+                if(freeCell % 2 == 0) {
+                    board [tileOfNumI(input)][tileOfNumJ(input)] = 2;
+                } else {
+                    board [tileOfNumI(input)][tileOfNumJ(input)] = 1;
+                }
             }
         }
-        draw();
-        if(checkWinner() == 1) {
-            cout << "O is the winner!";
-        } else if(checkWinner() == 2) {
-            cout << "X is the winner!";
+
+        draw(board);
+        cout << "The tile is unavalible!\n";
+        draw(board);
+        if(checkWinner(board) == 1) {
+            cout << "O is the winner!\n";
+        } else if(checkWinner(board) == 2) {
+            cout << "X is the winner!\n";
         } else {
-            cout << "Draw!";
+            cout << "Draw!\n";
         }
         //usr system ("pause"); for windows os
         pause();
